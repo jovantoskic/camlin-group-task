6c509f913ce3022e7e7bfefa8e3d0eca560d59d4
Jovan Toskic <jovan@yada.ai>
2025-06-10 11:41:32 +0200

    Fetch transformers on app initialization

--- camlin-task/src/App.vue
+++ camlin-task/src/App.vue
@@ -1,9 +1,24 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-    welcome
+  <main class="main-container">
+    <h1 class="headline">Transformer Dashboard</h1>
+    <TransformersTable />
+    <hr class="separator" />
+    <TransformersChart />
   </main>
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { useTransformerStore } from '@/stores/transformerStore';
+import TransformersTable from '@/components/TransformersTable.vue';
+import TransformersChart from '@/components/TransformersChart.vue';
+
+import '@/styles/app.scss';
+
+const store = useTransformerStore();
+
+onMounted(() => {
+  store.loadTransformers();
+});
+</script>
